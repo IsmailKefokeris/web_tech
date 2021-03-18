b188bac2b59b1b09f8999fe3d625e3336f487f62
ismailkefokeris <ismailkefokeris@gmail.com>
2021-03-18 09:56:32 +0000

    Form Validation

--- week6/barebones_webpage_example/scripts/formexample.js
+++ week6/barebones_webpage_example/scripts/formexample.js
@@ -1,4 +1,4 @@
-
+/**
 document.getElementById("btnsubmit").addEventListener("click", function(){
     let enteredName = document.getElementById("txtname").value;
     let enteredAge = document.getElementById("txtage").value;
@@ -7,3 +7,42 @@ document.getElementById("btnsubmit").addEventListener("click", function(){
 
     document.getElementById("result").innerHTML = output
 })
+*/
+
+document.getElementById("frmexample").addEventListener("submit", function(){
+    
+
+})
+
+function validate(){
+
+    let name = document.forms["frmexample"]["name"]
+    let age = document.forms["frmexample"]["age"]
+    let example = document.forms["frmexample"]["example"].value
+
+    let error = false;
+    let output = "";
+
+    if(name.trim() == ""){
+        output += "Please Enter Name <br>"
+        document.forms["frmexample"]["name"].style.backgroundColor = "red";
+        error = true;
+    }
+
+    if(age == ""){
+        output += "Please Enter Age <br>"
+        document.forms["frmexample"]["name"].style.backgroundColor = "red";
+        error = true;
+    }
+
+    if(example){
+        error = true;
+        output += "Please Pick one of the options!"
+    }
+
+    if(error){
+        document.getElementById("result").innerHTML = output
+        e.preventDefault();
+    }
+
+}
